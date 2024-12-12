750cdf0434f572a0cb8f8f451acce27d87d8306b
Tyler Kuehl <tylerkuehl99@gmail.com>
2024-12-11 18:34:48 -0600

    Working 1.20.4

---
+++ fabric/remappedSrc/einstein/usefulslime/platform/FabricPlatformHelper.java
@@ -0,0 +1,29 @@
+package einstein.usefulslime.platform;
+
+import einstein.usefulslime.platform.services.IPlatformHelper;
+import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.entity.LivingEntity;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+
+    @Override
+    public boolean isFakePlayer(LivingEntity entity) {
+        return entity instanceof FakePlayer;
+    }
+}
